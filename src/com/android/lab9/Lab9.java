fa1cfcdf0e355011366602c6ed5f32987594a607
WideLee <limkuan@mail2.sysu.edu.cn>
2013-05-03 21:26:27 +0800

    I don't know

---
+++ src/com/android/lab9/Lab9.java
@@ -0,0 +1,23 @@
+package com.android.lab9;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Lab9 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.lab9, menu);
+        return true;
+    }
+    
+}
